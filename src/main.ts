83de4248d76fd3cb56b102c8a171ab3240b850cb
Johnny Hao <lenhuthao0777@gmail.com>
2023-12-11 15:07:06 +0700

    fix env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const config = app.get(ConfigService);
+
+  app.use(cookieParser());
+
+  app.enableCors();
+
+  await app.listen(config.get('PORT'));
 }
 bootstrap();
