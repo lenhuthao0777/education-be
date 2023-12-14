f556efce6944af98be0e7c048dae9ef0118329ac
Johnny Hao <lenhuthao0777@gmail.com>
2023-12-14 21:23:48 +0700

    connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+
+import { PrismaService } from 'src/providers/prisma';
+import { UserModule } from 'src/api/v1/user/user.module';
+import { RoleModule } from 'src/api/v1/role/role.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UserModule,
+    RoleModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
